439e47eac20455bfa7929df6efddd4a45aaad479
Emmanuel Marabe Omokagbo <emmanuel.omokagbo@gmail.com>
2021-11-10 20:01:57 +0100

    create legal and system controllers

---
+++ MintynApp/Legal/SystemViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SystemViewController.swift
+//  MintynApp
+//
+//  Created by Emmanuel Omokagbo on 10/11/2021.
+//
+
+import UIKit
+
+class SystemViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
---
+++ MintynApp/System/LegalViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LegalViewController.swift
+//  MintynApp
+//
+//  Created by Emmanuel Omokagbo on 10/11/2021.
+//
+
+import UIKit
+
+class LegalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
